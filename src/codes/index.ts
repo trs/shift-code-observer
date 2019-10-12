d2b81f4e6f64766e8caa95354966250ea19e1021
Tyler Stewart <hello@tylerstewart.ca>
2019-10-11 18:41:02 -0600

    simplify api

--- src/codes/index.ts
+++ src/codes/index.ts
@@ -1,27 +1,20 @@
-import { merge } from 'rxjs';
+import { concat } from 'rxjs';
 import { distinct } from 'rxjs/operators';
 
 import { ShiftCode } from '../types';
-
+import {observe as orcicornHistoric} from './orcicorn-historic';
 import {observe as orcicorn} from './orcicorn';
 import {observe as tk} from './tk';
-
-import {observe as orcicornHistoric} from './orcicorn-historic';
 
 function distinctShiftCodes() {
   return distinct<ShiftCode, string>((code) => code.code);
 }
 
-export function getHistoric() {
-  return merge(
-    orcicornHistoric()
-  )
-    .pipe(distinctShiftCodes());
-}
-
-export function getCurrent() {
-  return merge(
+export function getShiftCodes() {
+  return concat(
+    orcicornHistoric(),
     orcicorn(),
     tk()
-  );
+  )
+    .pipe(distinct(distinctShiftCodes));
 }
